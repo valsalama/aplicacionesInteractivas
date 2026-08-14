f2ebcca3f63ae4e5f6e29f5abaf308f46be8ccd7
valsalama <vsalama@uade.edu.ar>
2026-08-13 21:22:12 -0300

    commit base proyecto

---
+++ e-commerce-jueves/src/main/java/com/uade/e_commerce/model/Producto.java
@@ -0,0 +1,20 @@
+package com.uade.e_commerce.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "productos")
+public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String descripcion;
+    private Double precio;
+}
